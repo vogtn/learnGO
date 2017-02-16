189cc381cf610418f19f1e834387847bad8cf75d
vogtn <nicolasjvogt@gmail.com>
2017-02-16 10:03:41 -0800

    adding string reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/vogtn/stringutil"
+)
 
 func main() {
-  fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
