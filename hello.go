8a97c5132ba871d613c5ccee7aa6edd57dcc227a
vogtn <nicolasjvogt@gmail.com>
2017-02-17 12:46:52 -0800

    adding get and set

--- hello.go
+++ hello.go
@@ -1,32 +1,85 @@
 package main
 
 import (
-  "encoding/json"
-	"fmt"
   "net/http"
+  "sync"
+  "net/url"
+  "fmt"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
-}
+
+var keyValueStore map[string]string
+var kVStoreMutex sync.RWMutex
 
 func main(){
-  http.HandleFunc("/", handler);
+  keyValueStore = make(map[string]string)
+  kVStoreMutex = sync.RWMutex{}
+  http.HandleFunc("/get", get)
+  http.HandleFunc("/set", set)
+  http.HandleFunc("/remove", remove)
+  http.HandleFunc("/list", list)
   http.HandleFunc("/about/", about)
-  http.ListenAndServe(":8080", nil);
+  http.ListenAndServe(":3000", nil);
 }
 
-type Message struct{
-  Text string;
+func get(w http.ResponseWriter, r *http.Request){
+  if(r.Method == http.MethodGet){
+    values, err := url.ParseQuery(r.URL.RawQuery)
+    if err != nil{
+      w.WriteHeader(http.StatusBadRequest)
+      fmt.Fprint(w, "Error:", err)
+      return
+    }
+    if len(values.Get("key")) == 0 {
+      w.WriteHeader(http.StatusBadRequest)
+      fmt.Fprint(w, "Error:", err)
+      return
+    }
+    kVStoreMutex.RLock()
+    value := keyValueStore[string(values.Get("key"))]
+    kVStoreMutex.RUnlock()
+
+    fmt.Fprint(w, value)
+  }else {
+    w.WriteHeader(http.StatusBadRequest)
+    fmt.Fprint(w, "Error: Only GET accepted.")
+  }
 }
 
-func about(w http.ResponseWriter, r *http.Request){
-  m := Message{"Welcome to the about section API"}
-   b, err := json.Marshal(m)
+func set(w http.ResponseWriter, r *http.Request){
+  if(r.method == http.MethodPost){
+    values, err := url.ParseQuery(r.URL.RawQuery)
+    if err != nil{
+      w.WriteHeader(http.StatusBadRequest)
+      fmt.Fprint(w, "Error:", err)
+      return
+    }
+    if len(values.Get("key")) == 0 {
+      w.WriteHeader(http.StatusBadRequest)
+      fmt.Fprint(w, "Error:", "wrong input key.")
+      return
+    }
+    if len(values.Get("value")) == 0 {
+      w.WriteHeader(http.StatusBadRequest)
+      fmt.Fprint(w, "Error:", "Wrong input value.")
+      returns
+    }
+    kvStoreMutex.Lock()
+    keyValueStore[string.Get("key")] =
+    string(values.Get("value"))
+    kVStoreMutex.Unlock()
 
-   if err != nil {
-       panic(err)
-   }
+    fmt.Fprint(w, "success")
+  }else{
+    w.WriteHeader(http.StatusBadRequest)
+    fmt.Fprint(w, "Error: Only POST accepted.")
+  }
+}
 
-    w.Write(b)
+func remove(w http.ResponseWriter, r *http.Request){
+
+}
+
+func list(w http.ResponseWriter, r *http.Request){
+
 }
