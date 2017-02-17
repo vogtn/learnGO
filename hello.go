2a766b384c30ee91adb067b33e30a6f02379133b
vogtn <nicolasjvogt@gmail.com>
2017-02-17 11:43:38 -0800

    finished basic API

--- hello.go
+++ hello.go
@@ -12,5 +12,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main(){
   http.HandleFunc("/", handler);
+  http.HandleFunc("/about/", about)
   http.ListenAndServe(":8080", nil);
+}
+
+type Message struct{
+  Text string;
+}
+
+func about(w http.ResponseWriter, r *http.Request){
+  m := Message{"Welcome to the about section API"}
+   b, err := json.Marshal(m)
+
+   if err != nil {
+       panic(err)
+   }
+
+    w.Write(b)
 }
